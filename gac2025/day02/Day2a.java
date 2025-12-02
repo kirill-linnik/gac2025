24046d7dc5fa05162b2da424e5f84732299ff551
Kirill Linnik <klinnik@microsoft.com>
2025-12-02 16:18:27 +0200

    day 2a

--- gac2025/day02/Day2a.java
+++ gac2025/day02/Day2a.java
@@ -1,16 +1,28 @@
 package gac2025.day02;
 
 import gac2025.Base;
-import java.util.List;
 
 public class Day2a extends Base {
     
     @Override
     public void solve() {
-        List<String> lines = readLines("gac2025/day02/input.txt");
-        
-        // TODO: Implement solution for Day 2 Part A
-        System.out.println("Day 2 Part A - Solution not yet implemented");
+        long result = 0;
+        String[] ranges = readFile("gac2025/day02/input-a.txt").split(",");
+        for (String range : ranges) {
+            String[] bounds = range.split("-");
+            long start = Long.parseLong(bounds[0]);
+            long end = Long.parseLong(bounds[1]);
+            for (long i = start; i <= end; i++) {
+                String number = String.valueOf(i);
+                if ( number.length() % 2 == 0 ){
+                    String firstHalf = number.substring(0, number.length() / 2);
+                    if ( number.endsWith(firstHalf) ){
+                        result += i;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
     
     public static void main(String[] args) {
