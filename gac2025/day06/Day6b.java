06d3f61bdb8ec0f113d64d554120e058d2372980
Kirill Linnik <kirill.linnik@mail.ee>
2025-12-06 08:32:30 +0200

    6a & 6b

--- gac2025/day06/Day6b.java
+++ gac2025/day06/Day6b.java
@@ -1,17 +1,69 @@
 package gac2025.day06;
 
 import gac2025.Base;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day6b extends Base {
     
     @Override
     public void solve() {
-        List<String> lines = readLines("gac2025/day06/input.txt");
+        List<String> lines = readLines("gac2025/day06/input-b.txt");
         
-        // TODO: Implement solution for Day 6 Part B
-        System.out.println("Day 6 Part B - Solution not yet implemented");
+        long result = 0;
+        
+        String lastLine = lines.get(lines.size() - 1);
+        char[] lastLineChars = lastLine.toCharArray();
+        int columnStart = 0;
+        int columnEnd = 0;
+        for ( int i = 0; i < lastLineChars.length; i++ ) {
+            char ch = lastLineChars[i];
+            if ( ch == ' ' ){
+                continue;
+            }
+
+            columnStart = i;
+            for ( int j = i + 1; j < lastLineChars.length; j++ ) {
+                char ch2 = lastLineChars[j];
+                if ( ch2 == ' ' ){
+                    columnEnd = j;
+                }
+                else {
+                    break;
+                }
+            }
+            
+            List<Long> columnNumbers = new ArrayList<>();
+            for ( int j = columnEnd; j >= columnStart; j-- ) {
+                StringBuilder sb = new StringBuilder();
+                for ( int k = 0; k < lines.size()-1; k++ ) {
+                    String line = lines.get(k);
+                    char ch1 = line.charAt(j);
+                    if ( ch1 == ' ' ){
+                        continue;
+                    }
+                    sb.append(ch1);
+                }
+                if ( sb.length() > 0 ){
+                    Long number = Long.parseLong(sb.toString());
+                    columnNumbers.add(number);
+                }
+            }
+            
+            if ( ch == '+' ){
+                long tempResult = columnNumbers.stream().mapToLong(Long::longValue).sum();
+                result += tempResult;
+            }
+            else if ( ch == '*' ){
+                long tempResult = columnNumbers.stream().reduce(1L, (a, b) -> a * b);
+                result += tempResult;
+            }
+        }
+
+        System.err.println("Result: " + result);
     }
+        
     
     public static void main(String[] args) {
         Day6b solution = new Day6b();
